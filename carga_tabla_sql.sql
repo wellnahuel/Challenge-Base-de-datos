02548c62a8e055a99a45077ccd3c9d8981d4f5d2
wellnahuel <98101202+wellnahuel@users.noreply.github.com>
2022-03-19 20:38:13 -0300

    Add files via upload

---
+++ carga_tabla_sql.sql
@@ -0,0 +1,22 @@
+INSERT INTO Municipalidad VALUES (1 ,'Mar del Plata');
+INSERT INTO Municipalidad VALUES (2 ,'Azul');
+INSERT INTO Municipalidad VALUES (3 ,'Tandil');
+INSERT INTO Municipalidad VALUES (4 ,'CABA');
+INSERT INTO Municipalidad VALUES (5 ,'Ituzaingo');
+INSERT INTO Municipalidad VALUES (6 ,'Rosario');
+INSERT INTO Municipalidad VALUES (7 ,'Las Nutrias');
+INSERT INTO Municipalidad VALUES (8 ,'Pehuajo');
+INSERT INTO Municipalidad VALUES (9 ,'Resistencia');
+INSERT INTO Municipalidad VALUES (10 ,'Jujuy');
+
+
+INSERT INTO Empleado VALUES (1 ,'Nahuel', 'Cittadino', '2234567894', 'sarasa@mail.com', 120000, 12, 'desarrollador', 1);
+INSERT INTO Empleado VALUES (1 ,'Nahuel', 'Cittadino', '2234567894', 'sarasa@mail.com', 120000, 12, 'desarrollador', 1);
+INSERT INTO Empleado VALUES (1 ,'Nahuel', 'Cittadino', '2234567894', 'sarasa@mail.com', 120000, 12, 'desarrollador', 1);
+INSERT INTO Empleado VALUES (1 ,'Nahuel', 'Cittadino', '2234567894', 'sarasa@mail.com', 120000, 12, 'desarrollador', 1);
+INSERT INTO Empleado VALUES (1 ,'Nahuel', 'Cittadino', '2234567894', 'sarasa@mail.com', 120000, 12, 'desarrollador', 1);
+INSERT INTO Empleado VALUES (1 ,'Nahuel', 'Cittadino', '2234567894', 'sarasa@mail.com', 120000, 12, 'desarrollador', 1);
+INSERT INTO Empleado VALUES (1 ,'Nahuel', 'Cittadino', '2234567894', 'sarasa@mail.com', 120000, 12, 'desarrollador', 1);
+INSERT INTO Empleado VALUES (1 ,'Nahuel', 'Cittadino', '2234567894', 'sarasa@mail.com', 120000, 12, 'desarrollador', 1);
+INSERT INTO Empleado VALUES (1 ,'Nahuel', 'Cittadino', '2234567894', 'sarasa@mail.com', 120000, 12, 'desarrollador', 1);
+INSERT INTO Empleado VALUES (1 ,'Nahuel', 'Cittadino', '2234567894', 'sarasa@mail.com', 120000, 12, 'desarrollador', 1);
